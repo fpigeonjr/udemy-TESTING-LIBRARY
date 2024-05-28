b9bbe1b587c76f6cd7178120fe3328243d2e6a5f
Frank Pigeon Jr <frank.pigeonjr@gmail.com>
2024-05-28 08:37:33 -0500

    complete intro section

--- vite-starter/src/App.test.jsx
+++ vite-starter/src/App.test.jsx
@@ -3,6 +3,6 @@ import App from "./App";
 
 test("App contains correct heading", () => {
   render(<App />);
-  const headingElement = screen.getByText(/learn react/i);
+  const headingElement = screen.getByRole('heading', {name: /learn react/i})
   expect(headingElement).toBeInTheDocument();
 });
