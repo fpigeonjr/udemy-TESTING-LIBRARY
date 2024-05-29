11a4c3b8d032e497891fea95b5f54e8d53f36401
Frank Pigeon Jr <frank.pigeonjr@gmail.com>
2024-05-28 20:54:58 -0500

    chore: Add gray button state and disable button functionality

--- vite-starter/src/App.jsx
+++ vite-starter/src/App.jsx
@@ -4,6 +4,8 @@ import './App.css';
 function App() {
   const [btnColor, setBtnColor] = React.useState('red');
   const [btnText, setBtnText] = React.useState('Change to blue');
+  const [isDisabled, setIsDisabled] = React.useState(false);
+  const className = isDisabled ? 'gray' : btnColor;
 
   const handleChange = () => {
     if (btnColor === 'red') {
@@ -14,10 +16,17 @@ function App() {
       setBtnText('Change to blue');
     }
   };
+  const handleCheckboxChange = () => {
+    setIsDisabled(!isDisabled);
+  };
 
   return (
     <>
-      <button className={btnColor} onClick={handleChange}>
+      <button
+        className={className}
+        onClick={handleChange}
+        disabled={isDisabled}
+      >
         {btnText}
       </button>
 
@@ -25,6 +34,7 @@ function App() {
         type="checkbox"
         id="disable-button-checkbox"
         defaultChecked={false}
+        onChange={handleCheckboxChange}
       />
       <label htmlFor="disable-button-checkbox">Disable button</label>
     </>
--- vite-starter/src/App.test.jsx
+++ vite-starter/src/App.test.jsx
@@ -22,13 +22,27 @@ describe('App', () => {
     expect(btn).toHaveTextContent(/red/i);
   });
 
-  test('checkbox flow', () => {
+  test('checkbox flow', async () => {
     render(<App />);
-
+    const user = userEvent.setup();
     const btn = screen.getByRole('button', { name: /blue/i });
     const checkbox = screen.getByRole('checkbox', { name: /disable button/i });
 
     expect(btn).toBeEnabled();
     expect(checkbox).not.toBeChecked();
+    await user.click(checkbox);
+    screen.debug();
+    // check to see if btn is disabled and class gray
+    expect(btn).not.toBeEnabled();
+    expect(checkbox).toBeChecked();
+    expect(btn).toHaveClass('gray');
+
+    await user.click(checkbox);
+    expect(btn).toBeEnabled();
+    expect(btn).toHaveClass('red');
+    expect(checkbox).not.toBeChecked();
+    // click btn to check class blue
+    await user.click(btn);
+    expect(btn).toHaveClass('blue');
   });
 });
